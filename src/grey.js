291394271e88e69e8b21ffd52a1ecb15d7c925b4
Adrian Smith <agnomen@gmail.com>
2017-05-27 12:29:45 -0400

    Draw the gradients

--- src/grey.js
+++ src/grey.js
@@ -2,7 +2,7 @@ var GREY = (function () {
     "use strict";
 
     function SpaceView() {
-        this.maximize = false;
+        this.maximize = true;
         this.updateInDraw = true;
 
         var self = this;
@@ -16,17 +16,60 @@ var GREY = (function () {
         this.space = null;
     }
 
+    function canvasMatching(image) {
+        var canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        return canvas;
+    }
+
+    function drawGradient(space, canvas, gradFunc) {
+        var context = canvas.getContext('2d');
+        var buffer = context.getImageData(0, 0, canvas.width, canvas.height);
+        for (var y = 0; y < space.height; ++y) {
+            for (var x = 0; x < space.width; ++x) {
+                var i = (y * space.width + x) * IMPROC.CHANNELS,
+                    result = gradFunc(space, x, y);
+                for (var c = 0; c < result.length; ++c) {
+                    buffer.data[i + c] = result[c];
+                }
+            }
+        }
+        context.putImageData(buffer, 0, 0);
+    }
+
+    function gradToPixel(grad) {
+        var c = Math.floor(((grad + 2)/ 4) * IMPROC.BYTE_MAX);
+        return [c, c, c, IMPROC.BYTE_MAX];
+    }
+
+    function xGradToPixel(space, x, y) {
+        return gradToPixel(space.gradient(x, y).x);
+    }
+
+    function yGradToPixel(space, x, y) {
+        return gradToPixel(space.gradient(x, y).y);
+    }
+
     SpaceView.prototype.processLevels = function () {
         var space =  new FIELD.Space(this.image.width, this.image.height);
         IMPROC.processImage(this.image, 0, 0, this.image.width, this.image.height, function (x, y, r, g, b, a) {
-            this.space.setPotential(x, y, r / IMPROC.BYTE_MAX);
+            space.setPotential(x, y, r / IMPROC.BYTE_MAX);
         });
+        space.computeGrads();
+
+        this.xGrad = canvasMatching(this.image);
+        this.yGrad = canvasMatching(this.image);
+
+        drawGradient(space, this.xGrad, xGradToPixel);
+        drawGradient(space, this.yGrad, yGradToPixel);
+
         this.space = space;
     }
 
     SpaceView.prototype.update = function (now, elapsed, keyboard, pointer) {
         if (this.space) {
-            this.space.update()
+            //this.space.update()
         }
     };
 
@@ -34,6 +77,10 @@ var GREY = (function () {
         context.clearRect(0, 0, width, height);
         if (this.batch.loaded) {
             BLIT.draw(context, this.image, 0, 0, BLIT.ALIGN.TopLeft);
+        }
+        if (this.space) {
+            BLIT.draw(context, this.xGrad, this.space.width, 0, BLIT.ALIGN.TopLeft);
+            BLIT.draw(context, this.yGrad, 0, this.space.height, BLIT.ALIGN.TopLeft);
         }
     };
 
