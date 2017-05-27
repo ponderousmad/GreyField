787e9fe82f7d294d633ef0d92b28f9bcd3c9680f
Jacob Siemons <j.j.siemons@gmail.com>
2017-05-27 10:01:10 -0400

    Changed tabs to spaces

--- src/field.js
+++ src/field.js
@@ -1,47 +1,48 @@
 var FIELD = (function () {
     "use strict";
-	function compute_gradients(scalar_field,x,y) {
-		var grad = new Float32Array(x * y * 2),
-			offset = x * y,
-			size = x * y * 2;
-		
-		for (var i = 0; i < offset; i++) {
-			if( ( (i-1) % y < (i+1) % y) {
-				grad[i] = scalar_field[i+1]-scalar_field[i-1];
-			} else {
-				grad[i] = 0;
-			}
-			
-			if( (i - y >= 0) && (i + y < offset)) {
-				grad[i+offset] = scalar_field[i+y] - scalar_field[i-y];
-			} else {
-				grad[i+offset] = 0;
-			}
-		}
-		return grad;
-	}
-	
-	function Ship (total_mass,empty_mass,particle_number,ejection_velocity,starting_position) {
-		//constants:
-		this.total_mass = total_mass;
-		this.empty_mass = empty_mass;
-		this.m_particle = (total_mass - empty_mass)/particle_number;
-		this.v_particle = ejection_velocity;
-		
-		//variables:
-		this.pos = starting_position;
-		this.vel = [0,0];
-		this.mass = total_mass;
-		
-	Ship.prototype.shoot = function(theta) {
-		vel[0] += cos(theta) * v_particle * m_particle / mass;
-		vel[1] += sin(theta) * v_particle * m_particle / mass;
-		mass -= m_particle;
-		//create new particles as well, going in opposite direction (-cos, -sin)
-		
-	}
-	
-	
+    function compute_gradients(scalar_field,x,y) {
+        var grad = new Float32Array(x * y * 2),
+            offset = x * y,
+            size = x * y * 2;
+        
+        for (var i = 0; i < offset; i++) {
+            if ( (i-1) % y < (i+1) % y) {
+                grad[i] = scalar_field[i+1]-scalar_field[i-1];
+            } else {
+                grad[i] = 0;
+            }
+            
+            if( (i - y >= 0) && (i + y < offset)) {
+                grad[i+offset] = scalar_field[i+y] - scalar_field[i-y];
+            } else {
+                grad[i+offset] = 0;
+            }
+        }
+        return grad;
+    }
+    
+    function Ship (total_mass,empty_mass,particle_number,ejection_velocity,starting_position) {
+        //constants:
+        this.total_mass = total_mass;
+        this.empty_mass = empty_mass;
+        this.m_particle = (total_mass - empty_mass)/particle_number;
+        this.v_particle = ejection_velocity;
+        
+        //variables:
+        this.pos = starting_position;
+        this.vel = new R2.V(0,0);
+        this.mass = total_mass;
+    }
+    
+    Ship.prototype.shoot = function(theta) {
+        this.vel.x += cos(theta) * this.v_particle * this.m_particle / this.mass;
+        this.vel.y += sin(theta) * this.v_particle * this.m_particle / this.mass;
+        this.mass -= this.m_particle;
+        //create new particles as well, going in opposite direction (-cos, -sin)
+        
+    }
+    
+    
     return {
 
     }
