aba22648f75e4e80758a2de07daf037480d36118
Jacob Siemons <j.j.siemons@gmail.com>
2017-05-27 09:49:41 -0400

    Changed classes to function / prototype style. Removed particle class as it is clutter at this point

--- src/field.js
+++ src/field.js
@@ -21,34 +21,24 @@ var FIELD = (function () {
 		return grad;
 	}
 	
-	class Ship {
-		constructor(total_mass,empty_mass,particle_number,ejection_velocity,starting_position) {
-			//constants:
-			this.total_mass = total_mass;
-			this.empty_mass = empty_mass;
-			this.m_particle = (total_mass - empty_mass)/particle_number;
-			this.v_particle = ejection_velocity;
-			
-			//variables:
-			this.pos = starting_position;
-			this.vel = [0,0];
-			this.mass = total_mass;
-		}
+	function Ship (total_mass,empty_mass,particle_number,ejection_velocity,starting_position) {
+		//constants:
+		this.total_mass = total_mass;
+		this.empty_mass = empty_mass;
+		this.m_particle = (total_mass - empty_mass)/particle_number;
+		this.v_particle = ejection_velocity;
 		
-		shoot(theta) {
-			vel[0] += cos(theta) * v_particle * m_particle / mass;
-			vel[1] += sin(theta) * v_particle * m_particle / mass;
-			mass -= m_particle;
-			//create new particles as well, going in opposite direction (-cos, -sin)
-			
-		}
-	}
-	
-	class Particle {
-		constructor(starting_position,starting_velocity) {
-			this.pos = starting_position;
-			this.vel = starting_velocity;
-		}
+		//variables:
+		this.pos = starting_position;
+		this.vel = [0,0];
+		this.mass = total_mass;
+		
+	Ship.prototype.shoot = function(theta) {
+		vel[0] += cos(theta) * v_particle * m_particle / mass;
+		vel[1] += sin(theta) * v_particle * m_particle / mass;
+		mass -= m_particle;
+		//create new particles as well, going in opposite direction (-cos, -sin)
+		
 	}
 	
 	
